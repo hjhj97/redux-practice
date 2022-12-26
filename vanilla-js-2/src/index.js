a398181b3bdac0e93ce0018f191a0f344a2e381a
JuHeonHa <hjhjaueon97@naver.com>
2022-12-26 17:58:47 +0900

    complete vanilla counter

--- vanilla-js-2/src/index.js
+++ vanilla-js-2/src/index.js
@@ -1,3 +1,31 @@
+import { createStore } from "redux";
+
 const add = document.querySelector("#add");
 const minus = document.querySelector("#minus");
-const span = document.querySelector("span");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+const countStore = createStore(countModifier);
+
+add.addEventListener("click", () => {
+  countStore.dispatch({ type: ADD });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: MINUS });
+});
+
+countStore.subscribe(() => {
+  number.innerText = countStore.getState();
+});
